c063b6800aaf521a4fea8cd19248117468cc4a0a
Drew Vogel <dvogel@sunlightfoundation.com>
2011-09-28 12:27:12 -0400

    hierarchy.py now searches up the family tree but doesn't search for other children of the ancestors.

--- duns/hierarchy.py
+++ duns/hierarchy.py
@@ -1,14 +1,54 @@
-import random
 import sys
 from django.db.models import Count
-from duns.models import FPDS, Name, DUNS
+from duns.models import DUNS
 from duns.views import DUNSBlacklist
 
 
 class Node(object):
     def __init__(self, nodevalue):
         self.value = nodevalue
-        self.children = []
+        self.children = set()
+
+    def __repr__(self):
+        return "<Node(%s, %r children)>" % (repr(self.value), len(self.children))
+
+
+class Graph(object):
+    def __init__(self):
+        self.roots = set()
+        self.nodes = {}
+
+    def add_node(self, value):
+        if value not in self.nodes:
+            node = Node(value)
+            self.nodes[value] = node
+            self.roots.add(node)
+        return self.nodes[value]
+
+    def add_edge(self, a, b):
+        if a in self.nodes and b in self.nodes:
+            node_a = self.nodes[a]
+            node_b = self.nodes[b]
+            node_a.children.add(node_b)
+            if node_b in self.roots:
+                self.roots.remove(node_b)
+
+    def find(self, needle):
+        if needle in self.nodes:
+            return self.nodes[needle]
+        else:
+            return None
+
+    def walk(self, visitor):
+        seen = set()
+        def _walk(n):
+            if n not in seen:
+                seen.add(n)
+                visitor(n)
+                for c in n.children:
+                    _walk(c)
+        for r in self.roots:
+            _walk(r)
 
 def duns_get_or_none(number):
     try:
@@ -16,82 +56,56 @@ def duns_get_or_none(number):
     except DUNS.DoesNotExist:
         return None
 
-def parents(duns, fiscal_year=None):
-    assert isinstance(duns, DUNS) == True
-    parent_query = duns.fpds
-    if isinstance(fiscal_year, int):
-        parent_query = parent_query.filter(fiscal_year=fiscal_year)
-    elif isinstance(fiscal_year, (str, unicode)):
-        parent_query = parent_query.filter(fiscal_year__in=fiscal_year)
-    parent_counts = parent_query.values('duns_parent').annotate(Count('duns_parent'))
-    parents = [duns_get_or_none(c['duns_parent']) for c in parent_counts]
-    return parents
+def ancestors(subject, graph):
+    assert isinstance(subject, DUNS) == True
+    if subject.number not in DUNSBlacklist:
+        parent_counts = subject.fpds.values('duns_parent').annotate(Count('duns_parent'))
+        parents = [duns_get_or_none(c['duns_parent']) for c in parent_counts]
+        for parent in parents:
+            if parent.number not in DUNSBlacklist and parent.number != subject.number:
+                graph.add_node(parent.number)
+                graph.add_edge(parent.number, subject.number)
+        for parent in parents:
+            if parent.number not in DUNSBlacklist and parent.number != subject.number:
+                ancestors(parent, graph)
+    return ancestors
 
-def children(duns, fiscal_year=None):
-    assert isinstance(duns, DUNS) == True
-    child_query = duns.fpds_parent
-    if isinstance(fiscal_year, int):
-        child_query = child_query.filter(fiscal_year=fiscal_year)
-    elif isinstance(fiscal_year, (str, unicode)):
-        child_query = child_query.filter(fiscal_year__in=fiscal_year)
-    child_counts = child_query.values('duns').annotate(Count('duns'))
-    children = [duns_get_or_none(c['duns']) for c in child_counts]
-    return children
+def descendants(subject, graph):
+    assert isinstance(subject, DUNS) == True
+    if subject.number not in DUNSBlacklist:
+        child_counts = subject.fpds_parent.values('duns').annotate(Count('duns'))
+        children = [duns_get_or_none(c['duns']) for c in child_counts]
+        for child in children:
+            if child.number not in DUNSBlacklist and child.number != subject.number:
+                graph.add_node(child.number)
+                graph.add_edge(subject.number, child.number)
+        for child in children:
+            if child.number not in DUNSBlacklist and child.number != subject.number:
+                descendants(child, graph)
 
-def related_duns(seed, fiscal_year=None):
+def related_duns(seed):
     assert isinstance(seed, DUNS) == True
-    queue = [seed]
-    duns = [seed]
+    graph = Graph()
+    graph.add_node(seed.number)
+    ancestors(seed, graph)
+    descendants(seed, graph)
+    return graph
 
-    if len(queue) > 0:
-        subject = queue.pop(0)
-        if subject.number in DUNSBlacklist:
-            print >>sys.stderr, 'Ignoring blacklisted %s' % subject.number
-        else:
-            print >>sys.stderr, "Considering %s" % subject.number
-            for d in children(subject, fiscal_year):
-                if d not in duns:
-                    if d.number in DUNSBlacklist:
-                        print >>sys.stderr, "Ignoring blacklisted %s" % d.number
-                    else:
-                        print >>sys.stderr, "Queueing child %s" % d.number
-                        queue.append(d)
-                        duns.append(d)
-            for d in parents(subject, fiscal_year):
-                if d not in duns:
-                    if d.number in DUNSBlacklist:
-                        print >>sys.stderr, "Ignoring blacklisted %s" % d.number
-                    else:
-                        #print >>sys.stderr, "Queueing parent %s" % d.number
-                        #queue.append(d)
-                        duns.append(d)
-    return duns
+def dump_dot(subject, graph):
+    def print_edges(node):
+        for c in node.children:
+            print "  %s -> %s;" % (node.value, c.value)
 
-def dump_dot(subject, dunslist):
-    edgelist = []
-    def add_edge(newedge):
-        (a, b) = newedge
-        if a != b:
-            if a not in DUNSBlacklist and b not in DUNSBlacklist:
-                if newedge not in edgelist:
-                    edgelist.append(newedge)
-    for duns in dunslist:
-        for p in parents(duns):
-            add_edge((p.number, duns.number))
-        for c in children(duns):
-            add_edge((duns.number, c.number))
     print "digraph dunsheirarchy {"
     print "  %s [style=filled,fontcolor=white,fillcolor=black];" % subject
-    for (index, (a, b)) in enumerate(edgelist):
-        print "  %s -> %s;" % (a, b)
+    graph.walk(print_edges)
     print "}"
 
 def main():
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         try:
             duns = DUNS.objects.get(number=arg)
-            print >>sys.stderr, 'Starting at %s' % duns.number
-            relatives = related_duns(duns, fiscal_year=None)
+            relatives = related_duns(duns)
             dump_dot(arg, relatives)
         except DUNS.DoesNotExist:
             print >>sys.stderr, 'Does not exist %s' % arg
