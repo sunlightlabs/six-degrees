b22c0d97891108cf8b29cefbd31364ae73f7e2da
Drew Vogel <dvogel@sunlightfoundation.com>
2011-09-30 15:18:14 -0400

    Added KEY_PREFIX to the memcache config.

--- settings.py
+++ settings.py
@@ -150,7 +150,8 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 60 * 60 * 24,
-        'MAX_ENTRIES': 10000
+        'MAX_ENTRIES': 10000,
+        'KEY_PREFIX': '6d'
     }
 }
 
